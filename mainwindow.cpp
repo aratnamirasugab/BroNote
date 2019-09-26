92aa04889de676d1bcb12fa790a4f12bc1909168
aratnamirasugab <officialbagus15@gmail.com>
2019-09-26 22:55:40 +0700

    added open file function

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -15,19 +15,25 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-
+//new file function
 void MainWindow::on_actionNew_triggered()
 {
     currentFile.clear();
     ui->textEdit->setText(QString());
 }
 
+//open file function
 void MainWindow::on_actionOpen_triggered()
 {
     QString filename = QFileDialog::getOpenFileName();
     QFile file(filename);
     currentFile = filename;
     if(!file.open(QIODevice::ReadOnly | QFile::Text)){
-        QMessageBox
+        QMessageBox::warning(this, "Warning", "Cannot open file : " + file.errorString());
     }
+    setWindowTitle(filename);
+    QTextStream in(&file);
+    QString text = in.readAll();
+    ui->textEdit->setText(text);
+    file.close();
 }
