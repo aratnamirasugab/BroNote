1e1cb5215fb744e986bffc7cdcea2bb6e74c174d
aratnamirasugab <officialbagus15@gmail.com>
2019-09-26 23:13:37 +0700

    added save as function

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -30,10 +30,30 @@ void MainWindow::on_actionOpen_triggered()
     currentFile = filename;
     if(!file.open(QIODevice::ReadOnly | QFile::Text)){
         QMessageBox::warning(this, "Warning", "Cannot open file : " + file.errorString());
+        return;
     }
     setWindowTitle(filename);
     QTextStream in(&file);
     QString text = in.readAll();
     ui->textEdit->setText(text);
+    file.close();
+}
+
+//save as function
+void MainWindow::on_actionSave_As_triggered()
+{
+    QString fileName = QFileDialog::getSaveFileName(this, "Save as");
+    QFile file(fileName);
+    if(!file.open(QFile::WriteOnly | QFile::Text)){
+        QMessageBox::warning(this, "Warning", "Cannot save file : " + file.errorString());
+        return;
+    }
+
+    //store fileName
+    currentFile = fileName;
+    setWindowTitle(fileName);
+    QTextStream out(&file);
+    QString text = ui->textEdit->toPlainText();
+    out << text;
     file.close();
 }
